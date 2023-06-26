87b7a1bbe4e7737c2d99cba6f3ed2808fde94690
Mallta <pggian1985@gmail.com>
2023-06-26 16:59:28 -0300

    config: heroku

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,14 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-   'coletor-de-codigo',
-   'postgres',
-   'postgres', {
-    host: 'localhost',
+   process.env.DB_DATABASE || 'coletor-de-codigo',
+   process.env.DB_USERNAME || 'postgres',
+   process.env.DB_PASSWORD || 'postgres', {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
     timezone: '-03:00',
     dialect: 'postgres',
+    protocol: 'postgres',
    }
 );
 
