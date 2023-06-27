6e9f527fe2d16c2c3c035e2edc7d119109659e1f
Mallta <pggian1985@gmail.com>
2023-06-27 17:37:37 -0300

    db config

--- src/database/index.js
+++ src/database/index.js
@@ -1,15 +1,27 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-   process.env.DB_DATABASE || 'coletor-de-codigo',
-   process.env.DB_USERNAME || 'postgres',
-   process.env.DB_PASSWORD || 'postgres', {
-      host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 5432,
+const DB_URI = "postgres://BrunoPogianMallta:QkZNLhAno37W@ep-jolly-butterfly-142296.us-east-2.aws.neon.tech/coletor-de-codigo;" // Defina a variável de ambiente DB_URI com a URI de conexão
+
+let sequelize;
+
+if (DB_URI) {
+  sequelize = new Sequelize(DB_URI, {
     timezone: '-03:00',
     dialect: 'postgres',
     protocol: 'postgres',
-   }
-);
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_DATABASE || 'coletor-de-codigo',
+    process.env.DB_USERNAME || 'postgres',
+    process.env.DB_PASSWORD || 'postgres', {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
+      timezone: '-03:00',
+      dialect: 'postgres',
+      protocol: 'postgres',
+    }
+  );
+}
 
 module.exports = sequelize;
