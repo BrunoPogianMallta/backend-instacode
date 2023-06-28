f25846caa2c48f2f67e2240285ad297d398cb851
Mallta <pggian1985@gmail.com>
2023-06-28 16:44:51 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 const corsOptions = {
-  origin: 'https://instacodehelper.cyclic.app'
+  origin: ['http://127.0.0.1:5500', 'https://instacodehelper.cyclic.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
